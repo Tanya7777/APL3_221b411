0bdcc7f69944866fe674a4d3ba1d58b1bb735789
221B411 <221B411@juet.co.in>
2025-03-27 12:17:12 +0530

    This is LAB 5

---
+++ LE5/TemplateMethod/Beverage.java
@@ -0,0 +1,19 @@
+abstract class Beverage{
+    private void pour(int qty){
+       System.out.println("Pour" + qty + "ml of beverage in a glass");
+    }
+    
+    protected abstract void addCondiment();
+    protected void stir(){ }
+    
+    private void serve(){
+        System.out.println("serve through waiter");
+    }
+    
+    public void templateMethod(int qty){
+        pour(qty);
+        addCondiment();
+        stir();
+        serve();
+        }
+}
