580bb5d1c542944c8642b348329fba72207f59c4
221B411 <221B411@juet.co.in>
2025-04-10 11:16:37 +0530

    lab 6 pushed

--- LE5/TemplateMethod/Whisky.java
+++
@@ -1,9 +0,0 @@
-class Whisky extends Beverage {
-    protected void addCondiment () {
-       System.out.println("Add some Ice");
-    }
-    
-    protected void stir (){
-          System.out.println("Stir for 30 sec");
-    }
-}
